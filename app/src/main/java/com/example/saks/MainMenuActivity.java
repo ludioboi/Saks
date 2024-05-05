6523576fe0aaf0a37b5a3db582aaf7de81d2e480
紗子 <88338845+Marcee2005@users.noreply.github.com>
2024-05-05 14:20:05 +0200

    New MainMenu with no working Fragments

--- app/src/main/java/com/example/saks/MainMenuActivity.java
+++ app/src/main/java/com/example/saks/MainMenuActivity.java
@@ -1,27 +1,133 @@
 package com.example.saks;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.saks.api.API_Access;
 import com.example.saks.databinding.ActivityMainMenuBinding;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private ActivityMainMenuBinding binding;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    showCamera();
+                }
+                else {
+                    Toast.makeText(this,"No Camera Permission", Toast.LENGTH_SHORT);
+                }
+            });
+
+    private  ActivityResultLauncher<ScanOptions> qrCodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+       if (result.getContents() == null) {
+           Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+       } else {
+           setResult(result.getContents());
+       }
+    });
+
+    private void setResult(String contents) {
+
+    }
+
+    private void showCamera() {
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+        options.setPrompt("Scan QR Code");
+        options.setCameraId(0);
+        options.setBeepEnabled(false);
+        options.setBarcodeImageEnabled(true);
+        options.setOrientationLocked(false);
+
+        qrCodeLauncher.launch(options);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        com.example.saks.databinding.ActivityMainMenuBinding binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        initBinding();
+        initViews();
+
         if (API_Access.token == null){
             Intent loginIntent = new Intent(this, LoginActivity.class);
             startActivity(loginIntent);
         }
     }
 
-    private void qrCode() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.bottom_nav_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.mmHome) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment,HomeFragment.newInstance("",""))
+                    .commit();
+        }
+        if (item.getItemId() == R.id.mmProfile) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment,ProfilFragment.newInstance("",""))
+                    .commit();
+        }
+        if (item.getItemId() == R.id.mmTimeTable) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment,TimeTableFragment.newInstance("",""))
+                    .commit();
+        }
+        if (item.getItemId() == R.id.mmSettings) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment,SettingsFragment.newInstance("",""))
+                    .commit();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initViews() {
+        binding.fab.setOnClickListener(view -> checkPermissionAndShowActivity(this));
+    }
+
+    private void checkPermissionAndShowActivity(Context context) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED) {
+            showCamera();
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            Toast.makeText(context, "Camera permission required", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
+    }
+
+    public void initBinding() {
+        binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
