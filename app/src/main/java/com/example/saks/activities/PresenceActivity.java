1b7e31efe015947a7d8b99d20b8b5c61d4fd6ab8
ludio_boi <fred@wilds-online.de>
2024-05-08 23:17:39 +0200

    API changes

--- app/src/main/java/com/example/saks/activities/PresenceActivity.java
+++ app/src/main/java/com/example/saks/activities/PresenceActivity.java
@@ -1,16 +1,26 @@
 package com.example.saks.activities;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.saks.R;
+import com.example.saks.api.API_Access;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class PresenceActivity extends AppCompatActivity {
 
@@ -33,6 +43,22 @@ public class PresenceActivity extends AppCompatActivity {
     public void handleUri(Uri uri){
         String data = uri.toString();
         TextView textView = findViewById(R.id.textView);
-        textView.setText(data);
+        textView.setTextColor(Color.RED);
+        textView.setText(uri.getPath() + "?" + uri.getQuery());
+        API_Access.postCall(uri.getPath() + "?" + uri.getQuery(), "{}", new Callback() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                textView.setTextColor(Color.GREEN);
+                textView.setText(response.body().string());
+            }
+        });
+
     }
 }
